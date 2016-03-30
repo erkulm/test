88da59e4047bb5958363e161a375deb7c66e788e
mahmut erkul <mahmuterkul1907@gmail.com>
2016-03-31 00:03:28 +0300

    Initial Commit

---
+++ project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Mahmut Erkul on 31/03/16.
+//  Copyright © 2016 Mahmut Erkul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
